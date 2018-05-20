ba4aaa78ce6c6a1ae7e17f7518d7a8a3bcd21d26
Max Hung <info@maxhung.co.nz>
2018-05-21 08:45:31 +1200

    Update maxhungry

--- keyboards/atreus/keymaps/maxhungry/keymap.c
+++ keyboards/atreus/keymaps/maxhungry/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_Q,    KC_W,    KC_E,          KC_R,    KC_T,    XXXXXXX,       KC_Y,          KC_U,           KC_I,     KC_O,          KC_P},
     {KC_A,    KC_S,    KC_D,          KC_F,    KC_G,    XXXXXXX,       KC_H,          KC_J,           KC_K,     KC_L,          KC_SCLN},
     {KC_Z,    KC_X,    KC_C,          KC_V,    KC_B,    GUI_T(KC_SPC), KC_N,          KC_M,           KC_COMM,  KC_DOT,        KC_SLSH},
-    {KC_LSFT, XXXXXXX, ALT_T(KC_TAB), LT(FUNC, KC_TAB), CTL_T(KC_ESC), ALT_T(KC_ENT), SFT_T(KC_BSPC), MO(NUMS), ALT_T(KC_TAB), XXXXXXX, XXXXXXX}
+    {KC_LSFT, XXXXXXX, ALT_T(KC_TAB), LT(FUNC, KC_TAB), CTL_T(KC_ESC), ALT_T(KC_ENT), SFT_T(KC_BSPC), LT(NUMS, KC_TAB), ALT_T(KC_TAB), XXXXXXX, XXXXXXX}
   },
   [NUMS] = {
     {KC_EXLM, KC_AT,   KC_LPRN, KC_RPRN, KC_PERC, XXXXXXX, KC_ASTR, KC_7,    KC_8, KC_9,    KC_MINS},
